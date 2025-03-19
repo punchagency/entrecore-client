a183d055d08bd6fa2f8c89df95f0f7429021620c
Adedoyin-Emmanuel <adedoyinemmanuel@punch.agency>
2025-03-19 12:20:18 +0100

    chore: modify commitlint configuration and update Husky scripts for improved compatibility

--- commitlint.config.js
+++ commitlint.config.js
@@ -19,7 +19,7 @@ module.exports = {
       ],
     ],
     "type-case": [2, "always", "lower-case"],
-    "subject-case": [2, "always", "lower-case"],
+    "subject-case": [0],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
     "body-leading-blank": [2, "always"],
