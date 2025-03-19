aee862f2938336e1e314af6e8225f7ed6e18b6ae
Adedoyin-Emmanuel <adedoyinemmanuel@punch.agency>
2025-03-19 13:09:23 +0100

    refactor: remove Login and Signup pages

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import LoginForm from "@/components/LoginForm";
 import Navbar from "@/components/Navbar";
+import SignupForm from "@/components/SignupForm";
 import { useRouter } from "next/navigation";
 
 const Login = () => {
@@ -9,12 +9,14 @@ const Login = () => {
 
   return (
     <div className="w-full h-screen flex flex-col bg-[#F0F4FF]">
-      <Navbar buttonText="Create Account" onButtonClick={() => router.push("/signup")} />
-      <div className="flex-1 flex items-center justify-center">
-        <LoginForm />
-      </div>
+    <Navbar buttonText="Login" onButtonClick={() => router.push("/auth/login")} />
+    <div className="flex-1 flex items-center justify-center">
+      <SignupForm />
     </div>
-  );
-};
+  </div>
+  )
+}
+
 
 export default Login;
+
