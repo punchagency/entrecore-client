5681c5d19f02e344a49483f3fc29d53cdf61701c
Adedoyin-Emmanuel <adedoyinemmanuel@punch.agency>
2025-03-19 12:54:25 +0100

    chore: update button component style

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,14 +9,12 @@ const Login = () => {
 
   return (
     <div className="w-full h-screen flex flex-col bg-[#F0F4FF]">
-    <Navbar buttonText="Create Account" onButtonClick={() => router.push("/signup")} />
-    <div className="flex-1 flex items-center justify-center">
-      <LoginForm />
+      <Navbar buttonText="Create Account" onButtonClick={() => router.push("/signup")} />
+      <div className="flex-1 flex items-center justify-center">
+        <LoginForm />
+      </div>
     </div>
-  </div>
-  )
-}
- 
+  );
+};
 
 export default Login;
-
