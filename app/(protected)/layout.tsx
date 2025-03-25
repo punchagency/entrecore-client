86d8b6aabb75cffbcdd2cef677140916d4cf52b0
Adedoyin-Emmanuel <adedoyinemmanuel@punch.agency>
2025-03-25 08:21:32 +0100

    chore: add page-header component

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,3 +1,4 @@
+import PageHeader from "@/components/page-header";
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 
@@ -5,7 +6,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full">{children}</main>
+      <main className="w-full">
+        <PageHeader />
+        {children}
+      </main>
     </SidebarProvider>
   );
 }
---
+++ components/page-header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageHeader = () => {
+  return (
+    <div className="w-full space-y-4">
+      <div className="flex items-center justify-between bg-[#EFF4FF] w-full px-2 py-4">
+        <h1 className="text-3xl font-bold">Metrics</h1>
+      </div>
+    </div>
+  );
+};
+
+export default PageHeader;
